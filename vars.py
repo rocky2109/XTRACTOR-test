93d63bba09a68789fb992613e2ee7a31c1b6af96
rocky2109 <rockys.2109rs@gmail.com>
2025-07-14 12:47:38 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,17 +1,15 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
-# Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
+API_ID = int(environ.get("API_ID", "123456"))
 API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", ""))
+OWNER = int(environ.get("OWNER", "0"))
+
 CREDIT = "<blockquote>[𝗖𝗛𝗢𝗦𝗘𝗡 𝗢𝗡𝗘 ⚝](http://t.me/CHOSEN_ONEx_bot)</blockquote>"
-AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
-AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-if int(OWNER) not in AUTH_USERS:
+
+AUTH_USER = os.environ.get('AUTH_USERS', '')
+AUTH_USERS = [int(user_id.strip()) for user_id in AUTH_USER.split(',') if user_id.strip().isdigit()]
+
+if str(OWNER).isdigit() and int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
-  
-#WEBHOOK = True  # Don't change this
-#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
